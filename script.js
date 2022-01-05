22129ca917b947ca23515b6706bda566dd092087
theMagnificentJay <reedjoshua408@gmail.com>
2022-01-05 15:29:22 -0600

    scss, gulp, style revisions, script.js creation, and all relevant linking

---
+++ script.js
@@ -0,0 +1,27 @@
+function removeAlert() {
+  document.getElementById("alert_notif").remove();
+}
+function btnOnClick1() {
+  var i = document.getElementById("button_series_1");
+  if (i.innerHTML == "- HIDE PACKAGE CONTENTS") {
+    i.innerHTML = "+ SEE PACKAGE CONTENTS";
+  } else {
+    i.innerHTML = "- HIDE PACKAGE CONTENTS";
+  }
+}
+function btnOnClick2() {
+  var i = document.getElementById("button_series_2");
+  if (i.innerHTML == "- HIDE PACKAGE CONTENTS") {
+    i.innerHTML = "+ SEE PACKAGE CONTENTS";
+  } else {
+    i.innerHTML = "- HIDE PACKAGE CONTENTS";
+  }
+}
+function btnOnClick3() {
+  var i = document.getElementById("button_series_3");
+  if (i.innerHTML == "- HIDE PACKAGE CONTENTS") {
+    i.innerHTML = "+ SEE PACKAGE CONTENTS";
+  } else {
+    i.innerHTML = "- HIDE PACKAGE CONTENTS";
+  }
+}
